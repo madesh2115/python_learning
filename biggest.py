4c26b561769c40d385fc26adcbbb3e77bf6b43c7
Madeshwaran p <madeshwaran.nkl@gmail.com>
2022-12-20 14:54:05 +0530

    find biggest number

---
+++ biggest.py
@@ -0,0 +1,14 @@
+#Find biggest number no1 and no2
+
+def biggest(no1, no2):
+	if no1>no2:
+		print("\n", no1, "is Biggest Number")
+	else:
+		print("\n" ,no2, "is Biggest Number")
+		
+
+no1 = int(input("\n Enter Number : "))
+
+no2 = int(input("\n Enter Number : "))
+
+function.biggest(no1,no2) 
